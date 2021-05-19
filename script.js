05a61c049a6e62035f315a99753e131f33ec7dd6
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2021-05-19 23:31:03 +0200

    some basic color added

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-function appendToList() {
-    const existingData = JSON.parse(localStorage.getItem('count'));
-    if (existingData.length == 0) {return};
-    
-    const lastItem = existingData[existingData.length - 1];
-    ul = document.getElementById('list');
+function appendToList(element) {
+    if (element.length == 0) {return};
+
+    var ol = document.getElementById('list');
     var li = document.createElement('li');
-    ul.appendChild(li);
-    li.innerHTML += lastItem;
+    ol.appendChild(li);
+    li.innerHTML += element;
 };
 
 function saveData() {
     const input = document.getElementById('count');
-    var existingData = JSON.parse(localStorage.getItem('count'));
+    if (input.value.length == 0) {return};
 
+    var existingData = JSON.parse(localStorage.getItem('count'));
     // if there is no object in local storage
     if (existingData == null) {
         var existingData = [];
@@ -21,18 +20,18 @@ function saveData() {
     existingData.push(input.value);
     localStorage.setItem('count', JSON.stringify(existingData));
 
-    appendToList()
+    appendToList(input.value)
 };
 
 function makeList() {
     const existingData = JSON.parse(localStorage.getItem('count'));
     if (existingData.length == 0) {return};
     
-    var ul = document.getElementById('list');
+    var ol = document.getElementById('list');
     
     existingData.forEach(element => {
         var li = document.createElement('li');
-        ul.appendChild(li);
+        ol.appendChild(li);
         li.innerHTML += element
     });
 }
