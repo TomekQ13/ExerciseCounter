d7cb83b28466c37ba52d3d9b2031deeefacbcc47
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2021-05-19 07:59:23 +0200

    list increments properly

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-function makeList() {
+function appendToList() {
     const existingData = JSON.parse(localStorage.getItem('count'));
     if (existingData.length == 0) {return};
     
-    ul = document.createElement('ul');
-    document.getElementById('stored-list').appendChild(ul);
-    
-    existingData.forEach(element => {
-        var li = document.createElement('li');
-        ul.appendChild(li);
-        li.innerHTML += element
-    });
-}
+    const lastItem = existingData[existingData.length - 1];
+    ul = document.getElementById('list');
+    var li = document.createElement('li');
+    ul.appendChild(li);
+    li.innerHTML += lastItem;
+};
 
 function saveData() {
     const input = document.getElementById('count');
@@ -24,6 +21,25 @@ function saveData() {
     existingData.push(input.value);
     localStorage.setItem('count', JSON.stringify(existingData));
 
-    makeList()
+    appendToList()
 };
 
+function makeList() {
+    const existingData = JSON.parse(localStorage.getItem('count'));
+    if (existingData.length == 0) {return};
+    
+    ul = document.getElementById('list');
+    
+    existingData.forEach(element => {
+        var li = document.createElement('li');
+        ul.appendChild(li);
+        li.innerHTML += element
+    });
+}
+
+function resetCounter() {
+    localStorage.removeItem('count');
+    
+}
+
+window.addEventListener('load', makeList())
