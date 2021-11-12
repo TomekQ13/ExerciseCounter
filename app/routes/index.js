932a3ebd4aebb7e1d16becd34ce6787606f39017
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2021-11-12 22:59:02 +0100

    nav

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,19 +7,4 @@ router.get("/", (req, res) => {
     res.render('index', { isAuthenticated: req.isAuthenticated() });
 });
 
-// router.get("/db/migrate", async (req, res) => {
-//     var trainings = Training.find();
-//     trainings.forEach((el) => {
-//         el.exercises.forEach((elem) => {
-//             elem.nameLowerCase = elem.name.toLowerCase()
-//         })
-//         el.markModified('exercises');
-//     })
-//     console.log('asdasd')
-//     await trainings.save()
-//     res.status(200).send('Create lower case names successfully')
-// });
-
-    
-
 module.exports = router
