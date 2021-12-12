ae8e737bbfa1b78222a2b8acb5592f62672218b5
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2021-12-12 22:23:21 +0100

    added bootstrap

--- app/public/script.js
+++ app/public/script.js
@@ -297,3 +297,12 @@ var cookieConsent = new CookieConsent({privacyPolicyUrl: "/privacy-policy.html"}
 
 
 
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
