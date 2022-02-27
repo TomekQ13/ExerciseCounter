e9ea095b62e35815b3bdec15b53b529d5389d2b2
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-02-27 23:08:54 +0100

    basic chart work

--- app/public/script.js
+++ app/public/script.js
@@ -370,4 +370,55 @@ if (dateInput) {
           form.classList.add('was-validated')
         }, false)
       })
-  })();
+})();
+
+const ctx = document.getElementById('myChart')
+async function makeChart() {
+    function getWeights() {
+        return fetch('/weight/values', {
+            method: "GET", 
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include'
+        })
+        .then(response => response.json())
+        .then(responseData => {
+            return responseData
+        })
+        .catch(error => console.error(error))
+    }
+    let resp = await getWeights()
+    const weights = resp.map(element => element.weightValue)
+    const dates = resp.map(element => element.date)
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: dates,
+            datasets: [{
+                label: 'Value',
+                data: weights,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        }
+    })    
+}
+if (ctx) {
+    makeChart()
+}
